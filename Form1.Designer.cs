6269114b87b29b639adcfeb41d516590a1a302da
Ylevo <4r2qobaf@anonaddy.me>
2023-08-31 06:47:53 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SpanishScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_download = new System.Windows.Forms.Button();
+            this.lbl_mangoUrl = new System.Windows.Forms.Label();
+            this.txtBox_mangoUrl = new System.Windows.Forms.TextBox();
+            this.txtBox_setFolder = new System.Windows.Forms.TextBox();
+            this.btn_setFolder = new System.Windows.Forms.Button();
+            this.setFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.listBox_Scannies = new System.Windows.Forms.CheckedListBox();
+            this.checkBox_MangoSubfolder = new System.Windows.Forms.CheckBox();
+            this.listbox_logger = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_download
+            // 
+            this.btn_download.Location = new System.Drawing.Point(21, 215);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(119, 23);
+            this.btn_download.TabIndex = 0;
+            this.btn_download.Text = "Download";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.Btn_download_Click);
+            // 
+            // lbl_mangoUrl
+            // 
+            this.lbl_mangoUrl.AutoSize = true;
+            this.lbl_mangoUrl.Location = new System.Drawing.Point(23, 39);
+            this.lbl_mangoUrl.Name = "lbl_mangoUrl";
+            this.lbl_mangoUrl.Size = new System.Drawing.Size(75, 15);
+            this.lbl_mangoUrl.TabIndex = 1;
+            this.lbl_mangoUrl.Text = "Mango URL :";
+            // 
+            // txtBox_mangoUrl
+            // 
+            this.txtBox_mangoUrl.Location = new System.Drawing.Point(104, 36);
+            this.txtBox_mangoUrl.Name = "txtBox_mangoUrl";
+            this.txtBox_mangoUrl.Size = new System.Drawing.Size(441, 23);
+            this.txtBox_mangoUrl.TabIndex = 2;
+            // 
+            // txtBox_setFolder
+            // 
+            this.txtBox_setFolder.Enabled = false;
+            this.txtBox_setFolder.Location = new System.Drawing.Point(104, 65);
+            this.txtBox_setFolder.Name = "txtBox_setFolder";
+            this.txtBox_setFolder.Size = new System.Drawing.Size(441, 23);
+            this.txtBox_setFolder.TabIndex = 4;
+            // 
+            // btn_setFolder
+            // 
+            this.btn_setFolder.Location = new System.Drawing.Point(22, 65);
+            this.btn_setFolder.Name = "btn_setFolder";
+            this.btn_setFolder.Size = new System.Drawing.Size(76, 23);
+            this.btn_setFolder.TabIndex = 5;
+            this.btn_setFolder.Text = "Set Folder";
+            this.btn_setFolder.UseVisualStyleBackColor = true;
+            this.btn_setFolder.Click += new System.EventHandler(this.Btn_setFolder_Click);
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.Location = new System.Drawing.Point(21, 119);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(121, 23);
+            this.btn_scan.TabIndex = 6;
+            this.btn_scan.Text = "Scan the scannies";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.Btn_scan_Click);
+            // 
+            // listBox_Scannies
+            // 
+            this.listBox_Scannies.FormattingEnabled = true;
+            this.listBox_Scannies.Location = new System.Drawing.Point(148, 119);
+            this.listBox_Scannies.Name = "listBox_Scannies";
+            this.listBox_Scannies.Size = new System.Drawing.Size(397, 76);
+            this.listBox_Scannies.TabIndex = 7;
+            this.listBox_Scannies.Visible = false;
+            // 
+            // checkBox_MangoSubfolder
+            // 
+            this.checkBox_MangoSubfolder.AutoSize = true;
+            this.checkBox_MangoSubfolder.Checked = true;
+            this.checkBox_MangoSubfolder.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_MangoSubfolder.Location = new System.Drawing.Point(23, 94);
+            this.checkBox_MangoSubfolder.Name = "checkBox_MangoSubfolder";
+            this.checkBox_MangoSubfolder.Size = new System.Drawing.Size(154, 19);
+            this.checkBox_MangoSubfolder.TabIndex = 9;
+            this.checkBox_MangoSubfolder.Text = "Create mango subfolder";
+            this.checkBox_MangoSubfolder.UseVisualStyleBackColor = true;
+            // 
+            // listbox_logger
+            // 
+            this.listbox_logger.FormattingEnabled = true;
+            this.listbox_logger.HorizontalScrollbar = true;
+            this.listbox_logger.ItemHeight = 15;
+            this.listbox_logger.Location = new System.Drawing.Point(148, 215);
+            this.listbox_logger.Name = "listbox_logger";
+            this.listbox_logger.Size = new System.Drawing.Size(397, 139);
+            this.listbox_logger.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Weird UI";
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(22, 255);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(118, 23);
+            this.btn_stop.TabIndex = 12;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 394);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listbox_logger);
+            this.Controls.Add(this.checkBox_MangoSubfolder);
+            this.Controls.Add(this.listBox_Scannies);
+            this.Controls.Add(this.btn_scan);
+            this.Controls.Add(this.btn_setFolder);
+            this.Controls.Add(this.txtBox_setFolder);
+            this.Controls.Add(this.txtBox_mangoUrl);
+            this.Controls.Add(this.lbl_mangoUrl);
+            this.Controls.Add(this.btn_download);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Spanish Scrapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_download;
+        private Label lbl_mangoUrl;
+        private TextBox txtBox_mangoUrl;
+        private TextBox txtBox_setFolder;
+        private Button btn_setFolder;
+        private FolderBrowserDialog setFolderDialog;
+        private Button btn_scan;
+        private CheckedListBox listBox_Scannies;
+        private CheckBox checkBox_MangoSubfolder;
+        private ListBox listbox_logger;
+        private Label label1;
+        private Button btn_stop;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+using HtmlAgilityPack;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml.XPath;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace SpanishScraper
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient httpClient;
+        private HtmlWeb webClient;
+        private HtmlDocument doc;
+        private bool canceled = false;
+        public Form1()
+        {
+            var socketsHttpHandler = new SocketsHttpHandler()
+            {
+                MaxConnectionsPerServer = 10
+            };
+            httpClient = new HttpClient(socketsHttpHandler);
+            webClient = new HtmlWeb();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0");
+            httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+            httpClient.DefaultRequestHeaders.Add("Referer", "https://lectortmo.com/");
+            InitializeComponent();
+            txtBox_setFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        }
+
+        private void Btn_setFolder_Click(object sender, EventArgs e)
+        {
+            if (setFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtBox_setFolder.Text = setFolderDialog.SelectedPath;
+            }
+        }
+
+        private void Btn_scan_Click(object sender, EventArgs e)
+        {
+            if (txtBox_mangoUrl.Text == String.Empty || txtBox_setFolder.Text == string.Empty)
+            {
+                MessageBox.Show("Retard.", "Retard Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+                return;
+            }
+            doc = webClient.Load(txtBox_mangoUrl.Text);
+            ListScantardsGroups(doc);
+        }
+        
+        private async void Btn_download_Click(object sender, EventArgs e)
+        {
+            if (!listBox_Scannies.Visible)
+            {
+                MessageBox.Show("You need to scan the scannies first to select your favourite autists.", "Scannies Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+                return;
+            }
+            toggleButtonsAndShit();
+            Dictionary<string, (string GroupName, string ChapterLink)[]> chapters = GetChaptersLinks(doc);
+            string mainFolder = txtBox_setFolder.Text;
+            string currentFolder;
+            if (checkBox_MangoSubfolder.Checked)
+            {
+                mainFolder = Path.Combine(mainFolder, ReplaceInvalidChars(txtBox_mangoUrl.Text.Split('/').Last()));
+                Directory.CreateDirectory(mainFolder);
+            }
+            try
+            {
+                foreach (var chapter in chapters.Reverse())
+                {
+                    addLog("Downloading chapter " + chapter.Key);
+                    currentFolder = Path.Combine(mainFolder, ReplaceInvalidChars(chapter.Key));
+                    Directory.CreateDirectory(currentFolder);
+                    foreach (var uploadedChapter in chapter.Value)
+                    {
+                        if (listBox_Scannies.CheckedItems.Contains(uploadedChapter.GroupName))
+                        {
+                            addLog("Downloading chapter by '" + uploadedChapter.GroupName + "'");
+                            await DownloadChapter(uploadedChapter.ChapterLink, Path.Combine(currentFolder, ReplaceInvalidChars(uploadedChapter.GroupName)));
+                            addLog("Done.");
+                        }
+                        if (canceled)
+                        {
+                            throw new TaskCanceledException();
+                        }
+                    }
+                    addLog("Done downloading chapter " + chapter.Key);
+                }
+
+            }
+            catch(TaskCanceledException)
+            {
+                addLog("Stopped download.");
+            }
+            catch(Exception exc)
+            {
+                addLog(exc.Message);
+                addLog("Something went wrong.");
+            }
+            finally
+            {
+                toggleButtonsAndShit();
+                listBox_Scannies.Visible = false;
+                listBox_Scannies.Items.Clear();
+            }
+        }
+
+        private async Task DownloadChapter(string chapterLink, string folderPath)
+        {
+            Directory.CreateDirectory(folderPath);
+            doc = webClient.Load(chapterLink);
+            string cascadeUrl = doc.DocumentNode.SelectSingleNode("//a[contains(@href, 'cascade')]").Attributes["href"].Value;
+            doc = webClient.Load(cascadeUrl);
+            var imgUrls = doc.DocumentNode.SelectNodes("//img[contains(concat(' ',normalize-space(@class),' '),' viewer-img ')]");
+            string url, filename;
+            var tasks = new List<Task>();
+            foreach (var imgUrl in imgUrls)
+            {
+                url = imgUrl.Attributes["data-src"].Value;
+                filename = url.Split('/').Last(); 
+                tasks.Add(DownloadFile(new Uri(url), Path.Combine(folderPath, filename), filename)); 
+                
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task DownloadFile(Uri uri, string path, string filename)
+        {
+            if (File.Exists(path)) 
+            {
+                addLog("Skipping file '" + filename + "' - already exists.");
+                return;
+            }
+            using (var fs = new FileStream(path, FileMode.CreateNew))
+            {
+                using (var s = await httpClient.GetStreamAsync(uri))
+                {
+                    addLog("Downloading file " + filename + " ...");
+                    await s.CopyToAsync(fs);
+                }
+            }
+        }
+
+        private void ListScantardsGroups(HtmlDocument doc)
+        {
+            List<string> scanGroups = new List<string>();
+            var scanGroupsNodes = doc.DocumentNode.SelectNodes(@"//li[contains(concat(' ',normalize-space(@class),' '),' upload-link ')]
+                                                                    //a[starts-with(@href, 'https://lectortmo.com/groups/')]");
+            foreach (var scanGroupNode in scanGroupsNodes)
+            {
+                scanGroups.Add(scanGroupNode.InnerText);
+            }
+            scanGroups = scanGroups.Distinct().ToList();
+            scanGroups.Sort();
+            listBox_Scannies.Items.Clear();
+            listBox_Scannies.Items.AddRange(scanGroups.ToArray());
+            listBox_Scannies.Visible = true;
+        }
+
+        private Dictionary<string, (string, string)[]> GetChaptersLinks(HtmlDocument doc)
+        {
+            Dictionary<string, (string, string)[]> chapters = new Dictionary<string, (string, string)[]>();
+            var chaptersNodes = doc.DocumentNode.SelectNodes("//li[contains(concat(' ',normalize-space(@class),' '),' upload-link ')]");
+            IEnumerable<HtmlNode> uploadedChaptersNodes;
+            IEnumerable<HtmlNode> uploadedChapterLinksNodes;
+            string chapterName;
+            for (int i = 0; i < chaptersNodes.Count; ++i)
+            {
+                chapterName = chaptersNodes[i].Descendants("a").First().InnerText.Trim();
+                uploadedChaptersNodes = chaptersNodes[i].Descendants("li");
+                (string, string)[] uploadedChapters = new (string, string)[uploadedChaptersNodes.Count()];
+                
+                for (int x = 0; x < uploadedChaptersNodes.Count(); ++x)
+                {
+                    uploadedChapterLinksNodes = uploadedChaptersNodes.ElementAt(x).Descendants("a");
+                    uploadedChapters[x] = (uploadedChapterLinksNodes.First().InnerText, uploadedChapterLinksNodes.Last().Attributes["href"].Value);
+                }
+                chapters.Add(chapterName, uploadedChapters);
+            }
+
+            return chapters;
+        }
+
+        private void toggleButtonsAndShit()
+        {
+            btn_setFolder.Enabled = !btn_setFolder.Enabled;
+            listBox_Scannies.Enabled = !listBox_Scannies.Enabled;
+            btn_scan.Enabled = !btn_scan.Enabled;
+            btn_download.Enabled = !btn_download.Enabled;
+            btn_stop.Enabled = !btn_stop.Enabled;
+            canceled = false;
+
+        }
+
+        private void addLog(string message)
+        {
+            listbox_logger.Items.Insert(0, message);
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            canceled = true;
+        }
+
+        private string ReplaceInvalidChars(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
